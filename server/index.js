4c54275a7cd21db2cbece79478fb5d9426565169
AkshayP-SH <akshayp@shnoor.com>
2026-08-25 18:20:17 +0530

    frontend initialized updated gitignore

--- server/index.js
+++ server/index.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<db_username>:hv5SxjhgCWgeuIBI@resolver.liy2vyf.mongodb.net/?appName=Resolver";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
